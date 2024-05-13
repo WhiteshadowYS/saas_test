80502dc2a3c94c884591bab3b2ee13be0bd87638
Shevelev Yurii <shevelev.yu@light-it.net>
2024-05-13 14:52:41 +0300

    feat(SCRIPTING): Add and Remove 80% complited
    Add and Remove 80% complited

--- scripts/enviroment.sh
+++ scripts/enviroment.sh
@@ -1,30 +1,86 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source "$SCRIPT_DIR/check_env.sh"
-default_home="$( cd "$SCRIPT_DIR/.." && pwd)"
-default_name=$(basename $default_home)
-PROJECT_HOME=$(echo "$default_name" | tr a-z A-Z)
-PROJECT_HOME_NAME="${PROJECT_HOME}_HOME"
-PROJECT_HOME=$(eval echo \$$PROJECT_HOME_NAME)
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#source "$SCRIPT_DIR/check_env.sh"
+#default_home="$( cd "$SCRIPT_DIR/.." && pwd)"
+#default_name=$(basename $default_home)
+#PROJECT_HOME=$(echo "$default_name" | tr a-z A-Z)
+#PROJECT_HOME_NAME="${PROJECT_HOME}_HOME"
+#PROJECT_HOME=$(eval echo \$$PROJECT_HOME_NAME)
 
-echo "Enviroment Setup"
+printf "\n"
+printf "Enviroment Setup\n"
 
 add() {
      printf "Adding active enviroments...\n"
      # Parse parrams for function
      while [ $# -gt 1 ]; do
      case "$2" in
-          -e|--env)
-          enviromentsMap="$3"
+          -n|--env_name)
+          name="$3"
+          ;;
+          -l|--env_link)
+          link="$3"
           ;;
      *)
           printf "Avaivalbe arguments: \n"
-          printf "       [-e|--env]*      - Map of enviroments with Git links [env_name, git_link] \n"    
+          printf "       [-n|--env_name]*      - Name of enviroment \n"    
+          printf "       [-l|--env_link]*      - Link to enviroment git \n"    
           exit 1
      esac
      shift
      shift
      done
+
+     # Exit if [name] was null
+     if [[ -z "$name" ]]; then
+          printf "\n"
+          printf "##########################################################   \n"
+          printf "\n"
+          printf "  [name] was null! Please set all requared params.          \n"
+          printf "  All requared params: env_name, env_link                    \n"
+          printf "  Use -h or --help to see all avaivable commands...           \n"
+          printf "\n"
+          printf "##########################################################  \n"
+          printf "\n"
+          printf "Enviroment Setup Failed...\n"
+          exit 1
+     fi
+     printf "Enviroment name: $name \n"
+
+      # Exit if [link] was null
+     if [[ -z "$link" ]]; then
+          printf "\n"
+          printf "##########################################################   \n"
+          printf "\n"
+          printf "  [link] was null! Please set all requared params.          \n"
+          printf "  All requared params: env_name, env_link                    \n"
+          printf "  Use -h or --help to see all avaivable commands...           \n"
+          printf "\n"
+          printf "##########################################################  \n"
+          printf "\n"
+          printf "Enviroment Setup Failed...\n"
+          exit 1
+     fi
+     printf "Enviroment link: $link \n"
+
+     printf "\n"
+     printf "Settuping variables...\n"
+     repository="https://github.com/WhiteshadowYS/saas_test.git"
+     pathToDownload="lib/sources"
+     pathToNewContentFolder="./$name"
+     mainFolderName="saas_test"
+     pathToCopy="$mainFolderName/lib/sources/main/."
+     printf "Setup Success!!!\n"
+
+     printf "\n"
+     printf "Downloading...\n"
+     cd $pathToDownload
+     git clone -b $link $repository
+     mkdir $pathToNewContentFolder
+     cp -R $pathToCopy $pathToNewContentFolder
+     rm -rf $mainFolderName
+     printf "Downloading Success!!!\n"
+     printf "\n"
 }
 
 remove() {
@@ -32,17 +88,46 @@ remove() {
      # Parse parrams for function
      while [ $# -gt 1 ]; do
      case "$2" in
-          -e|--env)
-          enviromentsList="$3"
+           -n|--env_name)
+          name="$3"
           ;;
      *)
           printf "Avaivalbe arguments: \n"
-          printf "       [-e|--env]*      - List of enviroment names what should be removed \n"
+          printf "       [-e|--env]*      - Enviroment name \n"
           exit 1
      esac
      shift
      shift
      done
+
+     # Exit if [name] was null
+     if [[ -z "$name" ]]; then
+          printf "\n"
+          printf "##########################################################   \n"
+          printf "\n"
+          printf "  [name] was null! Please set all requared params.          \n"
+          printf "  All requared params: env_name                          \n"
+          printf "  Use -h or --help to see all avaivable commands...           \n"
+          printf "\n"
+          printf "##########################################################  \n"
+          printf "\n"
+          printf "Enviroment Setup Failed...\n"
+          exit 1
+     fi
+     printf "Enviroment name: $name \n"
+
+     printf "\n"
+     printf "Settuping variables...\n"
+     pathToDownload="lib/sources"
+     pathToNewContentFolder="./$name"
+     printf "Setup Success!!!\n"
+
+     printf "\n"
+     printf "Removing...\n"
+     cd $pathToDownload
+     rm -rf $pathToNewContentFolder
+     printf "Removing Success!!!\n"
+     printf "\n"
 }
 
 
